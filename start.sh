f194d77d67e6f56d75696395a2b4f36c491e1ce4
Sean Xiao <seanxiao@augmentum.com.cn>
2019-04-03 17:23:11 +0800

    add builder container

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 init() {
-	echo "aaaaaaaaaaa"
+	echo "todo"
 }
 up() {
 	docker-compose up -d
 }
 build() {
-	docker run -it php:latest bash
+	docker run -it -v $PWD/backend:/srv/backend -v $PWD/frontend:/srv/frontend builder:latest bash
 }
 
 case "$1" in
