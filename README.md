739e12e2c674950de25058e5d15ebe91ac56f74d
Sean Xiao <seanxiao@augmentum.com.cn>
2019-04-11 13:42:11 +0800

    refine start.sh

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-./start.sh init 初始化
-./start.sh up 启动容器
-./start.sh build 编译代码
+进入项目根目录：
+    ./start.sh init 初始化，拉取容器镜像
+    ./start.sh up 启动容器
+    ./start.sh stop 停止容器
+    ./start.sh build 进入容器编译代码
--- start.sh
+++ start.sh
@@ -1,8 +1,18 @@
 init() {
-	echo "todo"
+	docker pull ubuntu:latest
+	docker pull mongo:latest
+	docker pull php:7.2-fpm
+	docker pull nginx:latest
+	sudo echo "127.0.0.1 api.test.com" >> /etc/hosts
+	sudo echo "127.0.0.1 frontend.test.com" >> /etc/hosts
 }
 up() {
+	cd $PWD/docker
 	docker-compose up -d
+}
+stop() {
+	cd $PWD/docker
+	docker-compose stop
 }
 build() {
 	docker run -it -v $PWD/backend:/srv/backend -v $PWD/frontend:/srv/frontend builder:latest bash
@@ -14,6 +24,9 @@ case "$1" in
 		;;
 	up)
 		up
+		;;
+	stop)
+		stop
 		;;
 	build)
 		build
